786023253a292724aa785e582e388f93e3fdf5e4
gustavobetete <gustavobetete@hotmail.com>
2023-08-03 16:40:11 -0300

    migration usuario

--- api/migrations/20230803162450-create-usuarios.js
+++ api/migrations/20230803162450-create-usuarios.js
@@ -6,7 +6,7 @@ module.exports = {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.UUIDV4,
+        type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4()
       },
       nome: {
