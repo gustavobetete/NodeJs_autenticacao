ead8d780a055561f8f001c63b4d04cbd9d87d5e9
gustavobetete <gustavobetete@hotmail.com>
2023-08-03 13:38:11 -0300

    criando rotas e tabelas

---
+++ api/routes/usuariosRoute.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.post('/usuarios', (req, res) => {});
+
+router.get('/usuarios', (req, res) => {});
+
+router.get('/usuarios/:id', (req, res) => {});
+
+router.put('/usuarios/:id', (req, res) => {});
+
+router.delete('/usuarios/:id', (req, res) => {});
+
+module.exports = router;
