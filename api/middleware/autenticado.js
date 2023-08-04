b1d0a3ae07d258999b36d57f3409186166d25f78
gustavobetete <gustavobetete@hotmail.com>
2023-08-04 15:13:37 -0300

    autenticação nas rotas

--- api/middleware/autenticado.js
+++ api/middleware/autenticado.js
@@ -4,8 +4,8 @@ const jsonSecret = require('../config/jsonSecret')
 module.exports = async (req, res, next) => {
     const token = req.headers.authorization
 
-    if (token) {
-        res.status(401).send('Access token nao informado')
+    if (!token) {
+       return res.status(401).send('Access token nao informado')
     }
 
     const [, accessToken] = token.split(" ")
