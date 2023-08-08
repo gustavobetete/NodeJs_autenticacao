015e3300f2990b895d230da430842d0ea2a32793
gustavobetete <gustavobetete@hotmail.com>
2023-08-08 14:39:20 -0300

    referencia e colocando cascade nas tabelas

--- api/migrations/20230808135212-create-usuarios-permissoes.js
+++ api/migrations/20230808135212-create-usuarios-permissoes.js
@@ -10,10 +10,22 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       usuario_id: {
-        type: Sequelize.UUID
+        type: Sequelize.UUID,
+        references: {
+          model: 'usuarios',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
       },
       permissao_id: {
-        type: Sequelize.UUID
+        type: Sequelize.UUID,
+        references: {
+          model: 'permissoes',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
       },
       createdAt: {
         allowNull: false,
