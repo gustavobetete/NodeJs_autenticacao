2a7c934baee052d0f270c7679a03451980147f04
gustavobetete <gustavobetete@hotmail.com>
2023-08-11 12:43:24 -0300

    cadastrar roles segurança routes

--- api/routes/segurancaRoute.js
+++ api/routes/segurancaRoute.js
@@ -6,5 +6,6 @@ const router = Router()
 router
     .post('/seguranca/acl', SegurancaController.cadastrarAcl)
     .post('/seguranca/permissoes-roles', SegurancaController.cadastrarPermissoesRoles)
+    .post('/seguranca/roles', SegurancaController.cadastrarRoles)
 
 module.exports = router
